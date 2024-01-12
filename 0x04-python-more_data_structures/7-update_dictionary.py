1947ea80672c0721435209ff1c485b65b0a526ea
gboigwe <geakande@gmail.com>
2024-01-12 11:47:23 -0800

    dict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    an_item = {key: value}
-    a_dictionary.update(an_item)
+    new_item = {key: value}
+    a_dictionary.update(new_item)
     return a_dictionary
